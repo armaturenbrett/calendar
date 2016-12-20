6b337ddd9766c59b0e7da331c33711a3723b824d
Robert Greinacher <robert@3antworten.de>
2016-12-20 14:41:22 +0100

    ical & caldav support

--- calendar.rb
+++ calendar.rb
@@ -29,37 +29,45 @@ class Calendar
 
   def find_upcoming_events
     events_of_all_calendars = []
-    @uris.each do |uri|
-      curl_request = `curl -X PROPFIND -u #{@user}:#{@password} -H "Content-Type: text/xml" -H "Depth: 1" --data "<propfind xmlns='DAV:'><prop><calendar-data xmlns='urn:ietf:params:xml:ns:caldav'/></prop></propfind>" #{uri}`
+
+    @uris['ical'].each do |uri|
+      curl_request = `curl -u "#{@user}":"#{@password}" #{uri}`
+      events_of_all_calendars << curl_request
+    end if @uris['ical'].present?
+
+    @uris['caldav'].each do |uri|
+      curl_request = `curl -X PROPFIND -u "#{@user}":"#{@password}" -H "Content-Type: text/xml" -H "Depth: 1" --data "<propfind xmlns='DAV:'><prop><calendar-data xmlns='urn:ietf:params:xml:ns:caldav'/></prop></propfind>" #{uri}`
 
       xml_document = Nokogiri::XML(curl_request)
       xml_document.remove_namespaces!
       events_of_one_calendar = xml_document.xpath('//calendar-data').map { |event| event.content }
       events_of_one_calendar = events_of_one_calendar.reject { |element| element.empty? }
-      events_of_all_calendars += events_of_one_calendar
-    end
+      events_of_all_calendars << events_of_one_calendar
+    end if @uris['caldav'].present?
 
-    filter_events(events_of_all_calendars)
+    filter_events(events_of_all_calendars.flatten)
   end
 
   def filter_events(raw_events)
     relevant_events = []
-    raw_events.each do |event|
-      parsed_event = Icalendar::Event.parse(event)[0]
-      next unless parsed_event
+    raw_events.each do |events|
+      parsed_events = Icalendar::Event.parse(events)
+      next unless parsed_events
 
-      start_at = DateTime.parse(parsed_event.dtstart.to_s)
-      end_at = DateTime.parse(parsed_event.dtend.to_s)
-      if end_at > DateTime.now
-        relevant_events << {
-          start_at: start_at,
-          start_time: create_time(start_at),
-          start_date: start_at.strftime(@date_format),
-          end_at: end_at,
-          end_time: create_time(end_at),
-          end_date: end_at.strftime(@date_format),
-          name: parsed_event.summary
-        }
+      parsed_events.each do |parsed_event|
+        start_at = DateTime.parse(parsed_event.dtstart.to_s)
+        end_at = DateTime.parse(parsed_event.dtend.to_s)
+        if end_at > DateTime.now
+          relevant_events << {
+            start_at: start_at,
+            start_time: create_time(start_at),
+            start_date: start_at.strftime(@date_format),
+            end_at: end_at,
+            end_time: create_time(end_at),
+            end_date: end_at.strftime(@date_format),
+            name: parsed_event.summary
+          }
+        end
       end
     end
 
@@ -81,15 +89,18 @@ end
 calendar = Calendar.new
 
 if __FILE__ == $0
-  # exit unless ARGV.count == 1
   calendar.cli_mode
   $config = {}
   $config['calendar'] = {}
-  $config['calendar']['uris'] = [
-    'https://www.google.com/calendar/dav/email@address.com/events',
+  $config['calendar']['uris'] = {}
+  $config['calendar']['uris']['ical'] = [
+    'https://calendar.google.com/calendar/ical/CALENDAR_ID/basic.ics'
   ]
-  $config['calendar']['user'] = 'username@address.com'
-  $config['calendar']['password'] = ARGV[0]
+  $config['calendar']['uris']['caldav'] = [
+    'https://nextcloud.example.de/remote.php/dav/calendars/USER/CALENDAR/'
+  ]
+  $config['calendar']['user'] = 'username'
+  $config['calendar']['password'] = 'password'
   $config['calendar']['upcoming_events'] = 5
   $config['calendar']['time_format'] = '%H:%M'
   $config['calendar']['date_format'] = '%d.%m'
